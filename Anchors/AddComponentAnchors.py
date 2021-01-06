84923504d57f6d647982d3a8905ed7f2c3b1df6e
protimient <benjones@protimient.com>
2021-01-06 17:15:44 +0000

    Added a non UI option to keep extant anchors

--- Anchors/AddComponentAnchors.py
+++ Anchors/AddComponentAnchors.py
@@ -7,6 +7,8 @@ Adds the anchors from all the components to all the layers in the current glyph.
 # Glyphs.clearLog()
 # Glyphs.showMacroWindow()
 
+keep_existing = False
+
 
 def get_component_anchors(layer, anchor_dict):
     for c in layer.components:
@@ -24,5 +26,7 @@ for g in [x.parent for x in Glyphs.font.selectedLayers]:
     for l in g.layers:
         anchors = get_component_anchors(l, {})
         for aname, position in anchors.items():
+            if keep_existing and l.anchors[aname] is not None:
+                continue
             newA = GSAnchor(aname, position)
             l.anchors[aname] = newA
