6a89cdd900170d65c8a3def4471181808f7a1d3f
protimient <benjones@protimient.com>
2021-01-20 18:30:03 +0000

    corrected menutitle

--- Layers/showCustomLayers.py
+++ Layers/showCustomLayers.py
@@ -1,4 +1,4 @@
-# MenuTitle: Show glyphs with custom layers (ignores special layers)
+# MenuTitle: Show glyphs with custom layers
 
 string = ' '.join(['/' + g.name for g in Glyphs.font.glyphs if len(g.layers) > len(Glyphs.font.masters)])
 if string:
