a087f7606b1f15cf7b3110d77e2e94c549120ff5
protimient <benjones@protimient.com>
2021-04-07 14:20:16 +0100

    Update DeleteSelectedAnchors.py

--- Anchors/DeleteSelectedAnchors.py
+++ Anchors/DeleteSelectedAnchors.py
@@ -1,10 +1,19 @@
 # MenuTitle: Delete ALL Anchors for Selected
 # -*- coding: utf-8 -*-
 __doc__ = """
-Deletes all anchors on every layer. Can be run on a single glyph in edit view or a selection of glyphs in font view.
+Deletes all anchors on the current layer, or with capslock on, every layer. Can be run on a single glyph in edit view or a selection of glyphs in font view.
 """
+# --- From mekkablue ---
+from AppKit import NSEvent
+keysPressed = NSEvent.modifierFlags()
+capslockKey = 65536
+capslockKeyPressed = keysPressed & capslockKey == capslockKey
+# ---
 
-for g in [x.parent for x in Glyphs.font.selectedLayers]:
-    for l in g.layers:
-        for i, aname in enumerate(l.anchors.keys()):
-            l.removeAnchorWithName_(aname)
+if capslockKeyPressed:
+    for g in [x.parent for x in Glyphs.font.selectedLayers]:
+        for l in g.layers:
+            l.anchors = []
+else:
+    for l in Glyphs.font.selectedLayers:
+        l.anchors = []
