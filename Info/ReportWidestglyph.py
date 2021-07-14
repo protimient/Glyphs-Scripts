0a84db23cc0c4a3eaeecc70caee6d3adabaf7ec9
protimient <benjones@protimient.com>
2021-07-14 16:15:35 +0100

    Update ReportWidestglyph.py

--- Info/ReportWidestglyph.py
+++ Info/ReportWidestglyph.py
@@ -6,4 +6,11 @@ print(widest.parent.name)
 
 Glyphs.showMacroWindow()
 
-# Glyphs.font.newTab(widest.parent.name)
+Glyphs.font.newTab([widest])
+Glyphs.font.currentTab.scale = 0.2
+Glyphs.font.currentTab.previewInstances = 'live'
+if len(Glyphs.font.tabs) == 1:
+    Glyphs.font.currentTab.previewHeight = 250
+else:
+    Glyphs.font.currentTab.previewHeight = Glyphs.font.tabs[0].previewHeight
+
